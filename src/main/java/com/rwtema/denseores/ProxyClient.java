670f1a9a785699abfc1b89f68ffec28229018fe7
rwtema <rwtema@gmail.com>
2014-08-08 10:54:32 +0100

    Added IdentifyBlock command to help configuration This will identify the block name/metadata/texture name of the currently help item. Also new stuff to localize.

--- src/main/java/com/rwtema/denseores/ProxyClient.java
+++ src/main/java/com/rwtema/denseores/ProxyClient.java
@@ -5,6 +5,7 @@ import net.minecraftforge.client.ClientCommandHandler;
 public class ProxyClient extends Proxy {
     @Override
     public void postInit() {
+        ClientCommandHandler.instance.registerCommand(new CommandIdentifyBlock());
         ClientCommandHandler.instance.registerCommand(new CommandClientOutputTextures());
     }
 }
