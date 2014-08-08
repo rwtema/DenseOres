27437b48b9984ed16b9118d533c8e3a08b09e94d
rwtema <rwtema@gmail.com>
2014-08-08 10:51:17 +0100

    Cleaned up the OutputTextures method

--- src/main/java/com/rwtema/denseores/CommandClientOutputTextures.java
+++ src/main/java/com/rwtema/denseores/CommandClientOutputTextures.java
@@ -28,55 +28,42 @@ public class CommandClientOutputTextures extends CommandBase {
         return "denseores.command.help";
     }
 
-    public String stripColon(String s) {
-        int i = s.indexOf(58);
-        if (i >= 0)
-            s = s.substring(i + 1, s.length());
-        return s;
-    }
-
     @Override
     public void processCommand(ICommandSender var1, String[] var2) {
         for (BlockDenseOre dense_ore_blocks : DenseOresRegistry.blocks.values()) {
             for (int meta = 0; meta < 16; meta++) {
-                if (dense_ore_blocks.isValid(meta)) {
-                    if (dense_ore_blocks.icons[meta] instanceof TextureOre) {
-                        TextureOre textureOre = (TextureOre) dense_ore_blocks.icons[meta];
+                if (dense_ore_blocks.isValid(meta) && dense_ore_blocks.icons[meta] instanceof TextureOre) {
+                    TextureOre textureOre = (TextureOre) dense_ore_blocks.icons[meta];
 
-                        BufferedImage image = textureOre.output_image;
-                        if (image == null)
+                    BufferedImage image = textureOre.output_image;
+                    if (image == null)
+                        continue;
+
+
+                    String s1 = "minecraft", s2 = textureOre.name;
+
+                    int ind = s2.indexOf(58);
+                    if (ind >= 0) {
+                        if (ind > 1) s1 = s2.substring(0, ind);
+                        s2 = s2.substring(ind + 1, s2.length());
+                    }
+                    s1 = s1.toLowerCase();
+
+                    File dir = new File(Minecraft.getMinecraft().mcDataDir, "denseoretextures");
+                    File moddir = new File(new File(new File(new File(new File(dir, "assets"), "denseores"), "textures"), "blocks"), s1);
+                    File f = new File(moddir, s2 + ".png");
+
+                    try {
+                        if (!f.getParentFile().exists() && !f.getParentFile().mkdirs())
+                            return;
+
+                        if (!f.exists() && !f.createNewFile())
                             continue;
 
-                        String s2 = textureOre.name;
-                        String s1 = "minecraft";
-
-                        int ind = s2.indexOf(58);
-
-                        if (ind >= 0) {
-                            if (ind > 1) {
-                                s1 = s2.substring(0, ind);
-                            }
-
-                            s2 = s2.substring(ind + 1, s2.length());
-                        }
-
-                        s1 = s1.toLowerCase();
-
-                        File dir = new File(Minecraft.getMinecraft().mcDataDir, "denseoretextures");
-                        File moddir = new File(new File(new File(new File(new File(dir, "assets"), "denseores"), "textures"), "blocks"), s1);
-                        File f = new File(moddir, s2 + ".png");
-
-                        try {
-                            f.getParentFile().mkdirs();
-                            if (!f.exists())
-                                f.createNewFile();
-
-                            ImageIO.write(image, "png", f);
-                        } catch (IOException e) {
-                            LogHelper.info("Unable to output " + textureOre.getIconName());
-                            e.printStackTrace();
-                            continue;
-                        }
+                        ImageIO.write(image, "png", f);
+                    } catch (IOException e) {
+                        LogHelper.info("Unable to output " + textureOre.getIconName());
+                        e.printStackTrace();
                     }
                 }
             }
